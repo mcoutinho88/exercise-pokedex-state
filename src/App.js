1f7f16ac74673890e561401f7a98abcb22602e07
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-15 19:28:45 -0300

    filter by type (fetching all types)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
-import pokemons from './data';
 import Pokedex from './Pokedex';
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+  return <Pokedex />;
 }
 
 export default App;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Pokemon from './Pokemon';
+import Button from './Button';
 
-class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
-    }
+import data from './data';
+
+export class Pokedex extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      type: 'Electric',
+    };
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(type) {
+    this.setState((state) => ({
+      type
+    }))
+  }
+
+  render() {
+    return (
+      <>
+        <h1 className="title">Pokedex</h1>
+        <div className="pokemon-container">
+          {data
+            .filter((poke) => poke.type === this.state.type)
+            .map((pokemon, index) => (
+              <Pokemon data={pokemon} key={pokemon.id} />
+            ))}
+        </div>
+        <div className="buttons">
+          <button onClick={() => this.handleClick('Electric')}>Electric</button> 
+          <button onClick={() => this.handleClick('Fire')}>Fire</button> 
+          <button onClick={() => this.handleClick('Bug')}>Bug</button> 
+          <button onClick={() => this.handleClick('Poison')}>Poison</button> 
+          <button onClick={() => this.handleClick('Psychic')}>Psychic</button> 
+          <button onClick={() => this.handleClick('Normal')}>Normal</button> 
+          <button onClick={() => this.handleClick('Dragon')}>Dragon</button> 
+        </div>
+      </>
+    );
+  }
 }
 
 export default Pokedex;
